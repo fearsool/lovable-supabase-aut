6128dca4b049233b4594e1010bd7b16ee90fa76f
fearsool <hagikurt@gmail.com>
2025-05-08 02:04:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+// Kalan kod...
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
